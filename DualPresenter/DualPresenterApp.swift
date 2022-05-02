939c57b00e17a2fbb7829b31082b624acf7bc91e
Andreas Pardeike <andreas.pardeike@polisen.se>
2022-05-02 09:38:24 +0200

    Initial Commit

---
+++ DualPresenter/DualPresenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DualPresenterApp.swift
+//  DualPresenter
+//
+//  Created by Andreas Pardeike on 2022-05-02.
+//
+
+import SwiftUI
+
+@main
+struct DualPresenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
